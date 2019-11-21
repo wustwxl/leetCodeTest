4e9ec7874c96b928bc4e487fbc54c7329be50cfd
wxl <1175141062@qq.com>
2019-11-21 18:22:33 +0800

    15. 注释

--- src/main/java/com/wust/algorithms/code15/ThreeSum.java
+++ src/main/java/com/wust/algorithms/code15/ThreeSum.java
@@ -13,4 +13,10 @@ public class ThreeSum {
 
         return null;
     }
+
+    public static void main(String[] args) {
+
+        int [] nums = new int[]{-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
 }
