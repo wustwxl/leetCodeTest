f53572ed250720abe018202e3a739c11dcaaa556
wxl <1175141062@qq.com>
2019-12-17 15:34:21 +0800

    定义饮料操作接口

---
+++ src/main/java/com/wust/factory/common/ColaDrinksOperationImpl.java
@@ -0,0 +1,16 @@
+package com.wust.factory.common;
+
+/**
+ * @author wxl
+ * @date 2019/12/17
+ * @description  定义具体的操作类
+ */
+public class ColaDrinksOperationImpl implements DrinksOperation {
+
+    @Override
+    public void makeDrinks() {
+
+        System.out.println("制作可乐饮料");
+    }
+
+}
---
+++ src/main/java/com/wust/factory/common/SpriteDrinksOperationImpl.java
@@ -0,0 +1,16 @@
+package com.wust.factory.common;
+
+/**
+ * @author wxl
+ * @date 2019/12/17
+ * @description  定义具体的操作类
+ */
+public class SpriteDrinksOperationImpl implements DrinksOperation {
+
+    @Override
+    public void makeDrinks() {
+
+        System.out.println("制作雪碧饮料");
+    }
+
+}
